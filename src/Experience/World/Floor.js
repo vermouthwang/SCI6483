7a56530979d447d5b233d63735fc98d9be24d0bd
vermouthwang <1184524959@qq.com>
2023-04-23 20:12:35 -0400

    after formed pointline

--- src/Experience/World/Floor.js
+++ src/Experience/World/Floor.js
@@ -21,23 +21,24 @@ export default class Floor
     setGeometry()
     {
         this.geometry = new THREE.PlaneGeometry(10, 10,10,10)
+        this.geometry2 = new THREE.PlaneGeometry(10, 10,10,10)
   
     }
 
     setTextures()
     {
-        this.textures = {}
+        // this.textures = {}
 
-        this.textures.color = this.resources.items.grassColorTexture
-        this.textures.color.encoding = THREE.sRGBEncoding
-        this.textures.color.repeat.set(1.5, 1.5)
-        this.textures.color.wrapS = THREE.RepeatWrapping
-        this.textures.color.wrapT = THREE.RepeatWrapping
+        // this.textures.color = this.resources.items.grassColorTexture
+        // this.textures.color.encoding = THREE.sRGBEncoding
+        // this.textures.color.repeat.set(1.5, 1.5)
+        // this.textures.color.wrapS = THREE.RepeatWrapping
+        // this.textures.color.wrapT = THREE.RepeatWrapping
 
-        this.textures.normal = this.resources.items.grassNormalTexture
-        this.textures.normal.repeat.set(1.5, 1.5)
-        this.textures.normal.wrapS = THREE.RepeatWrapping
-        this.textures.normal.wrapT = THREE.RepeatWrapping
+        // this.textures.normal = this.resources.items.grassNormalTexture
+        // this.textures.normal.repeat.set(1.5, 1.5)
+        // this.textures.normal.wrapS = THREE.RepeatWrapping
+        // this.textures.normal.wrapT = THREE.RepeatWrapping
     }
 
     setMaterial()
@@ -46,8 +47,15 @@ export default class Floor
             side: THREE.DoubleSide,
             transparent: true,
             wireframe :true,
-            color: new THREE.Color('#ff00ff'),
-            opacity: 0.5
+            color: new THREE.Color('#ffffff'),
+            opacity: 0.5,
+            wireframeLinewidth: 2
+        })
+        this.material2 = new THREE.MeshBasicMaterial({
+            side: THREE.DoubleSide,
+            color: new THREE.Color('#201020'),
+            opacity: 0.5,
+            wireframeLinewidth: 2
         })
         // this.material.opacity = 0.5
     }
@@ -55,10 +63,13 @@ export default class Floor
     setMesh()
     {
         this.mesh = new THREE.Mesh(this.geometry, this.material)
-        this.mesh.position.x = 0
+        this.mesh2 = new THREE.Mesh(this.geometry2, this.material2)
+        this.mesh.position.y = 0.01
         this.mesh.rotation.x = - Math.PI * 0.5
-        this.mesh.receiveShadow = true
+        this.mesh2.rotation.x = - Math.PI * 0.5
+        this.mesh2.receiveShadow = true
         this.scene.add(this.mesh)
+        this.scene.add(this.mesh2)
     }
 
     update()
